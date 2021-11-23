88f81d1e2b5629dc750e35701eb6954c3748bb65
VicHinga <36955977+VicHinga@users.noreply.github.com>
2021-11-23 10:08:39 +0300

    Add files via upload

---
+++ PowerShell/Adv-Function.ps1
@@ -0,0 +1,15 @@
+﻿# Advanced Functions in PowerShell
+# The function comes with many capabilities
+
+Function Adv-Function{
+
+# The functions in line 7 and 8 makes the function advanced
+[CmdletBinding()]
+Param([Parameter(Mandatory=$True)]$a, $b)
+
+# [Parameter(Mandatory=$True)] Make the parameter mandatory so that without it the function may not run
+
+Write-Host "This is an advanced function"
+Write-Host "$a $b"
+
+}
---
+++ PowerShell/Chart-Script.ps1
@@ -0,0 +1,45 @@
+﻿# Creating a powershell script for displaying data in a chart (From .Net Library)
+
+# Email: hinga.muchoki@strathmore.edu
+
+
+# We use a cmdlet called Add-Type (TO ADD ASSEMBLY)
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms.DataVisualization
+
+# 1.  Create a Windows Form
+
+$Form = New-Object System.Windows.Forms.Form 
+$Form.Width = 600
+$Form.Height = 600
+
+$Form.Controls.Add($Chart)
+
+    # 5. Additional info for the Chart
+$Chart.Titles.Add("Population of 5 Counties in Kenya ")
+$ChartArea.AxisX.Title = "5 Kenyan Counties"
+$ChartArea.AxisY.Title = "Population Count"
+    
+$Form.ShowDialog()
+
+# 2. Create a chart
+
+$Chart = New-Object System.Windows.Forms.DataVisualization.Charting.Chart
+$Chart.Width = 500
+$Chart.Height = 400
+$Chart.Left = 40
+
+# 3. Creating The Chart Area
+
+$ChartArea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
+$Chart.ChartAreas.Add($ChartArea)
+
+# 4. Adding Data to The Chart (Using Hash tables)
+
+$Counties = @{
+Kajiado = 687312; Kisumu = 968909; Lamu = 101539; Mombasa = 939370; Nairobi = 3138369 
+}
+ 
+$Chart.Series.Add("Data")
+$Chart.Series["Data"].Points.DataBindXY($Counties.Keys,$Counties.Values) 
